e3dc73ec150d7de81d586ee4fd429ca98b700930
sucf <1058772978@qq.com>
2023-07-11 21:52:35 +0800

    center cluster数据同步增加重试并能保证数据幂等

--- dao-cloud-center/src/main/java/com/junmo/center/core/cluster/DataSyncTask.java
+++ dao-cloud-center/src/main/java/com/junmo/center/core/cluster/DataSyncTask.java
@@ -31,30 +31,27 @@ public class DataSyncTask implements Runnable {
 
     @Override
     public void run() {
-        try {
-            DefaultPromise<Set<ServerNodeModel>> promise = new DefaultPromise<>(clusterCenterConnector.getChannel().eventLoop());
-            SyncClusterInformationResponseHandler.PROMISE_MAP.put(clusterSyncDataModel.getSequenceId(), promise);
-            clusterCenterConnector.syncData(clusterSyncDataModel);
-            if (!promise.await(3, TimeUnit.SECONDS) || !promise.isSuccess()) {
-                log.error("<<<<<<<<<<<<<< sync data to cluster error. already error count = {} >>>>>>>>>>>>>>", failMark);
+        while (true) {
+            try {
+                DefaultPromise<Set<ServerNodeModel>> promise = new DefaultPromise<>(clusterCenterConnector.getChannel().eventLoop());
+                SyncClusterInformationResponseHandler.PROMISE_MAP.put(clusterSyncDataModel.getSequenceId(), promise);
+                clusterCenterConnector.syncData(clusterSyncDataModel);
+                if (!promise.await(3, TimeUnit.SECONDS) || !promise.isSuccess()) {
+                    log.error("<<<<<<<<<<<<<< sync data to cluster error. already error count = {} >>>>>>>>>>>>>>", failMark);
+                    if (failMark >= 3) {
+                        log.error("data has been synchronized to the cluster more than 3 times");
+                        return;
+                    }
+                    failMark++;
+                }
+            } catch (Throwable e) {
+                log.error("an unexpected situation has been interrupted", e);
                 if (failMark >= 3) {
                     log.error("data has been synchronized to the cluster more than 3 times");
                     return;
                 }
                 failMark++;
-                SyncClusterInformationResponseHandler.PROMISE_MAP.put(clusterSyncDataModel.getSequenceId(), promise);
-                clusterCenterConnector.syncData(clusterSyncDataModel);
             }
-        } catch (Throwable e) {
-            log.error("an unexpected situation has been interrupted", e);
-            if (failMark >= 3) {
-                log.error("data has been synchronized to the cluster more than 3 times");
-                return;
-            }
-            failMark++;
-            DefaultPromise<Set<ServerNodeModel>> promise = new DefaultPromise<>(clusterCenterConnector.getChannel().eventLoop());
-            SyncClusterInformationResponseHandler.PROMISE_MAP.put(clusterSyncDataModel.getSequenceId(), promise);
-            clusterCenterConnector.syncData(clusterSyncDataModel);
         }
     }
 }
