394512892b423d71014758416a27449ea8fb77dc
sucf <1058772978@qq.com>
2024-01-10 17:56:15 +0800

    feat: gateway build

--- dao-cloud-core/src/main/java/com/junmo/core/netty/protocol/MessageType.java
+++ dao-cloud-core/src/main/java/com/junmo/core/netty/protocol/MessageType.java
@@ -45,6 +45,8 @@ public class MessageType {
         MESSAGE_TYPE_MAP.put(INQUIRE_CLUSTER_FULL_CONFIG_RESPONSE_MESSAGE, FullConfigModel.class);
         MESSAGE_TYPE_MAP.put(SYNC_CLUSTER_SERVER_REQUEST_MESSAGE, ClusterSyncDataRequestModel.class);
         MESSAGE_TYPE_MAP.put(SYNC_CLUSTER_SERVER_RESPONSE_MESSAGE, ClusterSyncDataResponseModel.class);
+        MESSAGE_TYPE_MAP.put(GATEWAY_REGISTER_ALL_SERVER_REQUEST_MESSAGE, GatewayPullServiceMarkModel.class);
+        MESSAGE_TYPE_MAP.put(GATEWAY_REGISTER_ALL_SERVER_RESPONSE_MESSAGE, GatewayServiceNodeModel.class);
     }
 
     public static Class<? extends Model> getMessageModel(byte messageType) {
