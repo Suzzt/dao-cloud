979f19892c7dca6c926952f977fe52ff405ca78a
sucf <1058772978@qq.com>
2024-01-12 13:47:24 +0800

    feat: gateway build registry.

--- dao-cloud-gateway/src/main/java/com/junmo/gateway/bootstrap/GatewayBootstrap.java
+++ dao-cloud-gateway/src/main/java/com/junmo/gateway/bootstrap/GatewayBootstrap.java
@@ -1,6 +1,10 @@
 package com.junmo.gateway.bootstrap;
 
-import com.junmo.boot.bootstrap.thread.SyncProviderServerTimer;
+import com.junmo.boot.bootstrap.manager.RegistryManager;
+import com.junmo.boot.properties.DaoCloudServerProperties;
+import com.junmo.core.model.RegisterProviderModel;
+import com.junmo.core.model.ServerNodeModel;
+import com.junmo.core.util.NetUtil;
 import com.junmo.core.util.ThreadPoolFactory;
 import com.junmo.gateway.bootstrap.thread.GatewayPullServiceTimer;
 import lombok.extern.slf4j.Slf4j;
@@ -31,9 +35,14 @@ public class GatewayBootstrap implements ApplicationListener<ContextRefreshedEve
         ThreadPoolFactory.GLOBAL_THREAD_POOL.execute(timer);
     }
 
+    /**
+     * registry gateway node to center
+     */
     public void registry() {
-
+        RegisterProviderModel gatewayNodeModel = new RegisterProviderModel();
+        gatewayNodeModel.setProxy("dao-cloud-gateway");
+        gatewayNodeModel.setProviderModels(null);
+        gatewayNodeModel.setServerNodeModel(new ServerNodeModel(NetUtil.getLocalIp(), DaoCloudServerProperties.serverPort));
+        RegistryManager.registry(gatewayNodeModel);
     }
-
-
 }
