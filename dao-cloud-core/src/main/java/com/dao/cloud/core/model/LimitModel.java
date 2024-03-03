af4a329d15583fc9d36d5c2290452a956aed6dda
sucf <1058772978@qq.com>
2024-03-03 22:10:08 +0800

    feat: dao-cloud gateway iteration(Complete gateway configuration model)

--- dao-cloud-core/src/main/java/com/dao/cloud/core/model/LimitModel.java
+++ dao-cloud-core/src/main/java/com/dao/cloud/core/model/LimitModel.java
@@ -16,6 +16,18 @@ public class LimitModel implements Serializable {
         this.limitNumber = limitNumber;
     }
 
+    public LimitModel(Integer limitAlgorithm, Long slideDateWindowSize, Integer slideWindowMaxRequestCount) {
+        this.limitAlgorithm = limitAlgorithm;
+        this.slideDateWindowSize = slideDateWindowSize;
+        this.slideWindowMaxRequestCount = slideWindowMaxRequestCount;
+    }
+
+    public LimitModel(Integer limitAlgorithm, Integer tokenBucketMaxSize, Integer tokenBucketRefillRate) {
+        this.limitAlgorithm = limitAlgorithm;
+        this.tokenBucketMaxSize = tokenBucketMaxSize;
+        this.tokenBucketRefillRate = tokenBucketRefillRate;
+    }
+
     /**
      * 限流算法
      */
@@ -26,9 +38,38 @@ public class LimitModel implements Serializable {
      */
     private Integer limitNumber;
 
+    /* ============================ 限流参数 ============================ */
+
     /**
-     * 滑动窗口大小
+     * 滑动时间窗口大小
      * (unit=ms)
      */
-    private Long slideWindowSize;
+    private Long slideDateWindowSize;
+
+    /**
+     * 时间窗口内允许的最大请求数
+     */
+    private Integer slideWindowMaxRequestCount;
+
+    /**
+     * 令牌桶的最大令牌数
+     */
+    private Integer tokenBucketMaxSize;
+
+    /**
+     * 每秒新增的令牌数
+     */
+    private Integer tokenBucketRefillRate;
+
+    /**
+     * 漏桶的容量
+     */
+    private Integer leakyBucketCapacity;
+
+    /**
+     * 令牌填充的速度(每秒)
+     */
+    private int leakyBucketRefillRate;
+
+    /* ============================ 限流参数 ============================ */
 }
