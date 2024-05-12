3085cb1e44f516b84d2d49a8a09d59a5ea888531
sucf <1058772978@qq.com>
2024-05-12 22:37:50 +0800

    fix: The service cannot be provided until the cluster service initialization is completed.

--- dao-cloud-center/src/main/java/com/dao/cloud/center/core/handler/AvailableHandler.java
+++ dao-cloud-center/src/main/java/com/dao/cloud/center/core/handler/AvailableHandler.java
@@ -1,15 +1,21 @@
 package com.dao.cloud.center.core.handler;
 
+import com.dao.cloud.center.bootstarp.DaoCloudCenterConfiguration;
 import com.dao.cloud.core.exception.DaoException;
+import com.dao.cloud.core.model.ErrorResponseModel;
 import com.dao.cloud.core.model.Model;
+import com.dao.cloud.core.netty.protocol.DaoMessage;
+import com.dao.cloud.core.netty.protocol.MessageType;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author: sucf
  * @date: 2024/4/25 22:54
  * @description: Node available handler
  */
+@Slf4j
 public class AvailableHandler extends SimpleChannelInboundHandler<Model> {
 
     private boolean available;
@@ -17,8 +23,14 @@ public class AvailableHandler extends SimpleChannelInboundHandler<Model> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Model msg) throws Exception {
         if (!available) {
-            // todo 这里要封装一个消息异常给到请求端
-            throw new DaoException("The current node is not ready");
+            ErrorResponseModel responseModel = new ErrorResponseModel();
+            responseModel.setDaoException(new DaoException("The current node is not ready"));
+            DaoMessage daoMessage = new DaoMessage((byte) 1, MessageType.ERROR_RESPONSE_MESSAGE, DaoCloudCenterConfiguration.SERIALIZE_TYPE, responseModel);
+            ctx.writeAndFlush(daoMessage).addListener(future -> {
+                if (!future.isSuccess()) {
+                    log.error("<<<<<<<<<<< Failed to send service configuration data >>>>>>>>>>>>", future.cause());
+                }
+            });
         }
         ctx.fireChannelRead(msg);
     }
