431679959bcb2eb4e90ace57cedbc9a6f36df4da
sucf <1058772978@qq.com>
2024-12-28 22:20:01 +0800

    feat: Configure the center to strengthen the capacity.

---
+++ dao-cloud-core/src/main/java/com/dao/cloud/core/model/ConfigurationShareClusterRequestModel.java
@@ -0,0 +1,13 @@
+package com.dao.cloud.core.model;
+
+/**
+ * configuration file share cluster request model
+ *
+ * @author sucf
+ * @create_time 2024/12/28 22:10
+ */
+public class ConfigurationShareClusterRequestModel  extends AbstractShareClusterRequestModel {
+    private String groupId;
+    private String fileName;
+    private String content;
+}
